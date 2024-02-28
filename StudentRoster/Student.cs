a185f5af054235e113b2ebadc54f6400bdd86db4
RichardRodi <RichardmRodi@gmail.com>
2024-02-28 11:31:02 -0500

    Finished step 2 and Created a student Class

---
+++ StudentRoster/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentRoster
+{
+    public class Student
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int ID { get; set; }
+
+        public List<int>Grades; 
+    }
+}
